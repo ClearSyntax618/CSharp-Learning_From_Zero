d6db5112b53e38c060fad0076c12dcfc3ed53353
Matias <141086020+ClearSyntax618@users.noreply.github.com>
2025-10-28 20:39:47 -0300

    update

--- main/FirstCSharpProgram.cs
+++ main/FirstCSharpProgram.cs
@@ -1,19 +1,70 @@
 ﻿using System; // Import the System namespace
 
-int x = 30 + 40;
-// System.Console.WriteLine(x);
-Console.WriteLine(x); // No need to specify System.
+class Program
+{
+	static void Main(string[] args)
+	{
 
-Console.WriteLine(FeetToInches(30)); // Lower-level functions
-Console.WriteLine(FeetToInches(40));
-SayHello();
+		int x = 30 + 40;
+		string message = "Hola Mundo";
+		int year = 2025;
+		string messageWithYear = message + " " + year.ToString();
 
-int FeetToInches (int feet) { // Higher-level functions
-	// Statement block
-	int inches = feet * 12;
-	return inches;
+		// System.Console.WriteLine(x);
+		Console.WriteLine(x); // No need to specify System.
+		Console.WriteLine(message);
+		Console.WriteLine(messageWithYear);
+
+		Console.WriteLine(FeetToInches(30)); // Lower-level functions
+		Console.WriteLine(FeetToInches(40));
+		SayHello();
+
+		/// ---------------------------------------
+		/// Built-in types
+		/// ---------------------------------------
+		bool exists = false;
+		if (exists)
+			Console.WriteLine("El valor existe");
+
+		int y = 5000;
+		bool lessThanFiveThousand = y < 5280;
+		if (lessThanFiveThousand)
+			Console.WriteLine("El valor de y es: " + y);
+
+		int FeetToInches(int feet)
+		{ // Higher-level functions
+		  // Statement block
+			int inches = feet * 12;
+			return inches;
+		}
+
+		void SayHello()
+		{
+			Console.WriteLine("Say 'Hello' to little friend!");
+		}
+
+		/// -------------------------------------
+		/// Custom Types
+		/// -------------------------------------
+		UnitConverter feetToInches = new UnitConverter(12);
+		Console.WriteLine(feetToInches.Convert(30));
+	}
 }
 
-void SayHello() {
-	Console.WriteLine("Say 'Hello' to little friend!");
+public class UnitConverter
+{
+	int ratio; // Fields
+
+	public UnitConverter(int unitRatio) // Constructor
+	{
+		ratio = unitRatio;
+	}
+
+	public int Convert(int unit) // Method
+	{
+		return unit * ratio;
+	}
+
 }
+
+
