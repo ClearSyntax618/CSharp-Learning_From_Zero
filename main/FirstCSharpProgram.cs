bf4026ebd02b8eaa1eece5888c477c3039dfee3b
Matias <141086020+ClearSyntax618@users.noreply.github.com>
2025-10-28 16:04:34 -0300

    archivo .cs

---
+++ main/FirstCSharpProgram.cs
@@ -0,0 +1,19 @@
+﻿using System; // Import the System namespace
+
+int x = 30 + 40;
+// System.Console.WriteLine(x);
+Console.WriteLine(x); // No need to specify System.
+
+Console.WriteLine(FeetToInches(30)); // Lower-level functions
+Console.WriteLine(FeetToInches(40));
+SayHello();
+
+int FeetToInches (int feet) { // Higher-level functions
+	// Statement block
+	int inches = feet * 12;
+	return inches;
+}
+
+void SayHello() {
+	Console.WriteLine("Say 'Hello' to little friend!");
+}
